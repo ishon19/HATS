250449c90072058a6afb07b3a4c49ac967f0d017
Shreyans Pathak <pathak.shreyans@gmail.com>
2021-11-29 19:50:24 -0500

    fix: Core name appended

--- Backend/solr.py
+++ Backend/solr.py
@@ -3,13 +3,12 @@
 '''
 
 import pysolr
-from constants import Constants
 
 
 class SolrServer:
     def __init__(self):
         self.solr = pysolr.Solr(
-            'http://localhost:8983/solr/'+Constants.SOLR_CORE_NAME, timeout=10, always_commit=True)
+            'http://localhost:8983/solr/IRF21P1', timeout=10, always_commit=True)
         self.solr.ping()
 
     def search_docs(self, query):
