784d3c8ceff70a069e1bac695479073fd1e0707b
Shreyans Pathak <pathak.shreyans@gmail.com>
2021-11-29 19:57:13 -0500

    fix: Logged response

--- Backend/solr.py
+++ Backend/solr.py
@@ -3,13 +3,16 @@
 '''
 
 import pysolr
+from constants import Constants
 
 
 class SolrServer:
     def __init__(self):
         self.solr = pysolr.Solr(
-            'http://localhost:8983/solr/IRF21P1', timeout=10, always_commit=True)
-        self.solr.ping()
+            'http://localhost:8983/solr/'+Constants.SOLR_CORE_NAME, timeout=10, always_commit=True)
 
     def search_docs(self, query):
-        return self.solr.search(query)
+        print("[search_docs] Search Query: ", query)
+        response = self.solr.search(query)
+        print("[search_docs] Response: ", response)
+        return response
