2e5c2a8f6c078698795b63f8d100cdceebc2aadd
Shreyans Pathak <pathak.shreyans@gmail.com>
2021-12-06 21:36:03 -0500

    feat: :stars: Sentiment analysis added

--- Backend/solr_utils.py
+++ Backend/solr_utils.py
@@ -2,6 +2,9 @@
     This file contains the utility methods for solr
 '''
 
+from sentiment_analyser import SentimentAnalyser
+
+
 class SolrUtils:
     '''
         Helper functions 
@@ -16,12 +19,17 @@ class SolrUtils:
             modified_query += 'tweet_text:'+token+' OR '
         modified_query = modified_query[:-4]
         return modified_query
-    
+
     def format_response(response):
         '''
             format the response
         '''
         formatted_response = []
         for doc in response:
+            temp_doc = []
+            temp_doc.append(doc)
+            sentimentAnalyser = SentimentAnalyser(doc)
+            doc = sentimentAnalyser.get_sentiment()
             formatted_response.append(doc)
+            print("[format_response] Doc: ", doc)
         return formatted_response
