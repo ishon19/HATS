297fe8b6859eb212cec178f347448229388a6502
Shreyans Pathak <pathak.shreyans@gmail.com>
2021-12-10 12:55:15 -0500

    fix: AWS IP

--- Backend/constants.py
+++ Backend/constants.py
@@ -4,4 +4,5 @@ class Constants:
     This class is for storing the constants used in the project.
     '''
     SOLR_CORE_NAME = 'DEMO_POI'
-    AWS_IP = '34.201.128.0'
+    # AWS_IP = '34.201.128.0'
+    AWS_IP = 'localhost'
