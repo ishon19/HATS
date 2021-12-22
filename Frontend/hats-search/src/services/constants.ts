0d9d209073737166afb00c0b06905958973e026c
Shreyans Pathak <pathak.shreyans@gmail.com>
2021-12-21 21:28:25 -0500

    fix: :wrench: endpoint update

--- Frontend/hats-search/src/services/constants.ts
+++ Frontend/hats-search/src/services/constants.ts
@@ -1,8 +1,8 @@
 // This file contains all the constants used by the services
 // export const APP_ENDPOINT = "http://18.205.244.23:9999";
 export const APP_ENDPOINT = "http://44.202.15.108:9999";
-export const SEARCH_ENDPOINT = "http://18.205.244.23:9999/search";
-export const FIND_TOP_POI_ENDPOINT = "http://18.205.244.23:9999/get-pois";
+export const SEARCH_ENDPOINT = "http://44.202.15.108:9999/search";
+export const FIND_TOP_POI_ENDPOINT = "http://44.202.15.108:9999/get-pois";
 export const infectedUrl =
   "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 export const recoveredUrl =
